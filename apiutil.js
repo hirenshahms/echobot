d930dfd27e7c4628e471bf1d83f0b985f96e82ce
hirenshahms <hirenshah_ms@hotmail.com>
2017-03-09 22:22:59 -0800

    Update

--- apiutil.js
+++ apiutil.js
@@ -1,7 +1,7 @@
 var request = require('bluebird').promisifyAll(require('request'));
 
 var url = 'https://products-r12.apptio.com/biit/api/v2/domains/reference.apptio.com/projects/Cost Transparency/reports/33d03665-aafd-4931-bed9-832ac55c64f1/dates/Jun:FY2016/components/4157';
-var auth = 'auth=-lx5lc58plhT3op71Z0IfQ';
+var auth = 'auth=N7NdQyurwdMYkY1Zx4w6Jg';
 
 module.exports = {
 
